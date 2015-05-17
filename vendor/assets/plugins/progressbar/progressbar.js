52a5a4f1bef57280a3be134bb040c5d32dc77d81
Louis Pham <louisitvn@gmail.com>
2015-05-17 22:55:00 +0700

    Init with Rails 4.2.1 and default stuffs

---
+++ vendor/assets/plugins/progressbar/progressbar.js
@@ -0,0 +1,17 @@
+/**
+ * ProgressBar for jQuery
+ *
+ * @version 1 (29. Dec 2012)
+ * @author Ivan Lazarevic
+ * @requires jQuery
+ * @see http://workshop.rs
+ *
+ * @param  {Number} percent
+ * @param  {Number} $element progressBar DOM element
+ */
+function progressBar(percent, $element) {
+	var progressBarWidth = percent * $element.width() / 100;
+	// $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
+  // by Nghi
+  $element.find('div').animate({ width: progressBarWidth }, 0).html(percent + "%&nbsp;");
+}
